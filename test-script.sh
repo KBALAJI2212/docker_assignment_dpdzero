8d46b575919c63fdc6e5111472d0f8ec6a7d0f8d
KBALAJI2212 <balaji2212@hotmail.com>
2025-06-25 00:08:36 +0530

    Created a bash script to automate Health checks

--- test-script.sh
+++ test-script.sh
@@ -14,14 +14,27 @@ N="\e[0m"
 
 
 validation(){
-    if [ $1 -eq 0 ];
+    local url="$1"
+    local message="$2"
+
+    status=$(curl -s -o dev/null -w"%{http_code}" "$url")
+
+    if [ "$status" -eq 200 ];
     then 
-        echo -e "${G}$2 is successful${N}"
+        echo -e "${G}$message is successful${N}"
     else 
-        echo -e "${R}$2 has Failed${N}"
-        exit 1;
+        echo -e "${R}$message has Failed${N}"
     fi
 }
 
 echo -e "${Y}Testing Go:Ping${N}" 
-if [ ]
+validation "http://localhost:8080/service1/ping" "Testing Go:Ping"
+
+echo -e "${Y}Testing Go:Hello${N}" 
+validation "http://localhost:8080/service1/hello" "Testing Go:Hello"
+
+echo -e "${Y}Testing Python:Ping${N}" 
+validation "http://localhost:8080/service2/ping" "Testing Python:Ping"
+
+echo -e "${Y}Testing Python:Hello${N}" 
+validation "http://localhost:8080/service2/hello" "Testing Python:Hello"
